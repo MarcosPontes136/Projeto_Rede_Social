3d8376dbe515a5d4340f966d9c8ca59995a90400
MarcosPontes136 <zeuscod@outlook.com>
2022-04-21 18:05:55 -0300

    Update class Events

--- src/main/java/com/redeSocial/projeto/entities/Follow/Follow.java
+++ src/main/java/com/redeSocial/projeto/entities/Follow/Follow.java
@@ -1,5 +1,10 @@
 package com.redeSocial.projeto.entities.Follow;
 
-public class Follow {
+import com.redeSocial.projeto.entities.user.User;
 
+public class Follow extends User{
+
+	public Follow(String name, String birthdate, String relationship ) {
+		super(name, birthdate, relationship);
+	}
 }
--- src/main/java/com/redeSocial/projeto/entities/marketplace/Marketplace.java
+++ src/main/java/com/redeSocial/projeto/entities/marketplace/Marketplace.java
@@ -69,7 +69,4 @@ public class Marketplace {
 			System.out.println();
 		}
 	}
-	
-	
-
 }
