262f4899eec60c6a1e4f0ad74fee0e923fc96f78
MarcosPontes136 <marcos.ponts@hotmail.com>
2022-05-22 16:49:45 -0300

    Update class MarketplaceDAO

--- src/main/java/com/redeSocial/projeto/db/MarketplaceDAO.java
+++ src/main/java/com/redeSocial/projeto/db/MarketplaceDAO.java
@@ -2,6 +2,9 @@ package com.redeSocial.projeto.db;
 
 import java.util.List;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+
 import com.redeSocial.projeto.entities.Marketplace;
 
 
@@ -9,26 +12,40 @@ public class MarketplaceDAO implements InterfaceDAO<Marketplace> {
 
 	@Override
 	public void persiste(Marketplace t) {
-		// TODO Auto-generated method stub
+		EntityManager em = UtilDB.getEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(t);
+			em.getTransaction().commit();
+		} catch (EntityExistsException e) {
+			em.getTransaction().rollback();
+			Marketplace original = get(t.getProductName());
+			em.getTransaction().begin();
+			original.setProductPrice(t.getProductPrice());
+			original.setProductDescription(t.getProductDescription());
+			em.getTransaction().commit();
+		}
 		
 	}
 
 	@Override
 	public void remove(Marketplace t) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = UtilDB.getEntityManager();
+		em.getTransaction().begin();
+		em.remove(t);
+		em.getTransaction().commit();		
 	}
 
 	@Override
 	public Marketplace get(Object pk) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = UtilDB.getEntityManager();
+		Marketplace t = em.find(Marketplace.class, pk);
+		return t;
 	}
 
 	@Override
 	public List<Marketplace> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return UtilDB.getEntityManager().createQuery("SELECT u FROM Marketplace u", Marketplace.class).getResultList();
 	}
 
 }
